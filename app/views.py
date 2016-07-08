19d83799f8f7036646245b72aec3b2e9bea85a86
Greg Emmanuel Alenton <gremmanuelenton02@gmail.com>
2016-07-08 12:21:19 +0800

    added templates

--- app/views.py
+++ app/views.py
@@ -1,15 +1,5 @@
-from flask import *
-from flask_login import *
-from .forms import * 
+from flask import render_template, redirect, url_for
+from flask_login import login_required, current_user
+from app import app
+from .forms import LoginForm
 
-
-@app.route('/')
-@login_required
-def index():
-	return render_template("")
-
-@app.route('/login')
-def login():
-	form = LoginForm()
-	
-	return render_template("")
