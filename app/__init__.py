c88fe0ab101e1b20bc10ca8a647cf38a24a91235
Bo Malicay <mslmalicay@gmail.com>
2016-07-29 16:53:18 +0800

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -10,11 +10,19 @@ app = Flask(__name__, instance_relative_config=True)
 
 #configurations
 db = SQLAlchemy(app)
+login_manager = LoginManager()
+login_manager.init_app(app)
 app.config.from_envvar('APP_SETTINGS', silent=True)
 app.config.from_object('config')
 app.config.from_pyfile('config.py')
 
-#app.database = "sample.db"
-
 from app import views
-#from app import models
+from models import User
+from db_create import *
+from db_create_users import *
+
+login_manager.login_view = "login"
+
+@login_manager.user_loader
+def load_user(user_id):
+  return User.query.filter(User.id == int(user_id)).first()
