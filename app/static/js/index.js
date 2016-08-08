e95f4d37da4f1e8fdc65df8169ab595803c1eb57
Greg Emmanuel Alenton <gremmanuelenton02@gmail.com>
2016-08-08 10:41:55 +0800

    updated templates

--- app/static/js/index.js
+++ app/static/js/index.js
@@ -2,3 +2,13 @@
 $(function () {
   $('[data-toggle="tooltip"]').tooltip()
 })
+
+// button function 
+$('.reply').click(function(){
+	$(this).hide();
+	$('.email-content').css()
+});
+
+$('.cancel').click(function(){
+	$('.reply').show(500);
+});
