a04e93897862b7d3a423944f5d3be1e6c3f77d44
Greg Emmanuel Alenton <gremmanuelenton02@gmail.com>
2016-08-08 11:47:14 +0800

    changed lead names

--- sql.py
+++ sql.py
@@ -40,10 +40,10 @@ def addLead(clientID, leadName, leadPhoneNumber, leadEmail, leadInquiry):
 #dummy data
 #addClient('Bo Malicay', 'NASA-lupa', 'bomalicay', sha256_crypt.encrypt("secret"))	
 addLead('1', 'Juan dela Cruz', '09324284362', 'juandelacruz@gmail.com', 'How much is the 2 bedroom condo? What are the payment terms?')
-addLead('1', 'Teborsyo Hampaslupa', '09425346787', 't_hampaslupa@hotmail.com', 'I\'m very much interested with the 1 bedroom condo with balcony. Is Pag-Ibig applicable with this?')
-addLead('2', 'Pedro Bahaghari', '09324567890', 'pedrobahaghari@gmail.com', 'When is the turnover? What are the payment terms? The downpayment?')
+addLead('1', 'John Smith', '09425346787', 'john.smith@hotmail.com', 'I\'m very much interested with the 1 bedroom condo with balcony. Is Pag-Ibig applicable with this?')
+addLead('2', 'Alice Green', '09324567890', 'agreen@gmail.com', 'When is the turnover? What are the payment terms? The downpayment?')
 addLead('2', 'Marco Buttondown', '09321112233', 'marcobuttons@hotmail.com', 'Do you do in-house financing? Would like to invest in 3-bedroom condo.')
-addLead('2', 'Juan Tamad', '09324445567', 'juantamad@yahoo.com', 'How do I invest? What are the requirements?')
+addLead('2', 'Clark Santos', '09324445567', 'CSantos@yahoo.com', 'How do I invest? What are the requirements?')
 addLead('1', 'Juan dela Cruz', '09324284362', 'juandelacruz@gmail.com', 'How much is the 2 bedroom condo? What are the payment terms?')
 addLead('1', 'John Smith', '09425346787', 'john.smith@hotmail.com', 'I am very much interested with the 1 bedroom condo with balcony. Is Pag-Ibig applicable with this?')
 addLead('1', 'Alice Green', '09324567890', 'aliceg@gmail.com', 'When is the turnover? What are the payment terms? The downpayment?')
